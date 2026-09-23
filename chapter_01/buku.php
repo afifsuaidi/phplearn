bbb5d7db96f918e2102000648f69675f093751de
afifsuaidi <121703117+afifsuaidi@users.noreply.github.com>
2026-09-23 19:49:52 +0700

    complete chapter 1

---
+++ chapter_01/buku.php
@@ -0,0 +1,12 @@
+<?php
+
+$judul = "Belajar PHP";
+$penulis = "Afif Suaidi";
+$kategori = "Pemrograman";
+$target = "1 Buku Selesai";
+
+echo "<h1>Daftar Buku</h1>";
+echo "<p>Judul : $judul</p>";
+echo "<p>Penulis : $penulis</p>";
+echo "<p>Kategori : $kategori</p>";
+echo "<p>Target : $target</p>";
---
+++ chapter_01/profil.php
@@ -0,0 +1,11 @@
+<?php
+
+$nama = "Afif Suaidi";
+$hobi = "Membaca dan Literasi";
+$target = "100 buku";
+
+echo "<h1>Profil Saya</h1>";
+
+echo "<p>Nama: $nama</p>";
+echo "<p>Hobi: $hobi</p>";
+echo "<p>Target: $target tahun ini</p>";
